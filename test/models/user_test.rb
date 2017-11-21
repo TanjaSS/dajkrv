10e9e2f4ea8c26a32853d6fcc9aff2d520cbfb24
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-11-21 22:31:36 +0100

    Extract setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(email: "tanja@example.com",
+                     password: "password",
+                     password_confirmation: "password")
+  end
+
+  test "valid user" do
+    assert @user.valid?
+  end
+
   test "presence of email" do
-    user = User.new
-    user.validate
-
-    assert user.errors[:email].present?
-
-    user.email = "tanja@example.com"
-    user.validate
-    
-    assert user.errors[:email].blank?
+    @user.email = ""
+    assert_not @user.valid?
+    assert @user.errors[:email].present?
   end
 end
