aa7d44362b62405e9ce8a29c8df1e6ca96f9c7bd
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-11-22 12:09:58 +0100

    Show alert for wrong password

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,6 +6,9 @@ class LoginsController < ApplicationController
     user = User.find_by(email: params[:login][:email])
     if user.authenticate(params[:login][:password])
       session[:user_id] = user.id
+    else
+      flash[:danger] = "Prijava nije uspela"
+      redirect_to root_path
     end
   end
 end
--- test/system/sign_ins_test.rb
+++ test/system/sign_ins_test.rb
@@ -10,4 +10,14 @@ class SignInsTest < ApplicationSystemTestCase
     click_button "Prijavi se"
     assert_content "Uspešno ste se prijavili"
   end
+
+  test "invalid password" do
+    mika = users(:mika)
+    visit new_login_path
+    fill_in "Email", with: mika.email
+    fill_in "Lozinka", with: "wrongpassword"
+    click_button "Prijavi se"
+    assert_content "Prijava nije uspela"
+    assert_link "Prijavi se"
+  end
 end
