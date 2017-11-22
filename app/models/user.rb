40c9ed59d791b0f31f6f279b24e2d0ae24211b54
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-11-22 10:58:15 +0100

    Add email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   has_secure_password
 
   before_save :downcase_email
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true
 
   private
