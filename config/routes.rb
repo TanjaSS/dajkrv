4e1d22fde1edbb553099e257872233b5bebefddc
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-11-28 13:05:38 +0100

    Add settings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'logins/new'
-
   root "homepage#index"
   resources :users, only: [:new, :create]
   resource :login, only: [:new, :create, :destroy]
+  resource :settings, only: [:edit]
 end
