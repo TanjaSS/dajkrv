19e5bfd00a1dad50bfb3fd0c697e31bfbc50bc08
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-11-30 12:20:47 +0100

    Add date select

--- test/system/settings_test.rb
+++ test/system/settings_test.rb
@@ -10,10 +10,14 @@ class SettingsTest < ApplicationSystemTestCase
     click_button "Prijavi se"
 
     select "6", from: "Koliko često želiš da daješ krv?"
-    fill_in "Poslednja donacija", with: "2017-11-11"
+    select "25", from: "user_last_donation_at_3i"
+    select "Maj", from: "user_last_donation_at_2i"
+    select "2017", from: "user_last_donation_at_1i"
     click_button "Sačuvaj"
     assert_content "Uspešno ste sačuvali podešavanja"
-    assert_field "Poslednja donacija", with: "2017-11-11"
+    assert_select "user_last_donation_at_3i", selected: "25"
+    assert_select "user_last_donation_at_2i", selected: "Maj"
+    assert_select "user_last_donation_at_1i", selected: "2017"
     assert_select "Koliko često želiš da daješ krv", selected: "6"
   end
 end
