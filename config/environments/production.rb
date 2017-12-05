a3952e1b0c7661265e910099f0569c2bfae9ae2e
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-12-05 11:30:40 +0100

    Setup SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,15 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.smtp_settings = {
+    :address => ENV["SMTP_ADDRESS"],
+    :domain => ENV["SMTP_DOMAIN"],
+    :user_name => ENV["SMTP_USERNAME"],
+    :password => ENV["SMTP_PASSWORD"],
+    :port => 587,
+    :authentication => :plain
+   }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
