c7891e8202fc4263ad60068eb51feadf5bdf80a3
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-12-06 09:14:30 +0100

    Add admin attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130101941) do
+ActiveRecord::Schema.define(version: 20171206081137) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171130101941) do
     t.datetime "updated_at", null: false
     t.integer "period"
     t.date "last_donation_at"
+    t.boolean "admin", default: false
   end
 
 end
