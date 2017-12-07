c74c6317117e1cb6711b3b31afad7084c804eb55
Tanja Stjepanovic Stricevic <stricevictanja@gmail.com>
2017-12-07 09:19:50 +0100

    List all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :verify_user, only: [:index]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "homepage#index"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:index, :new, :create]
   resource :login, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
 end
